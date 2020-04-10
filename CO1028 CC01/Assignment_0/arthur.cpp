166320a83784c7139c7bf67f54dc40df5a4dcb65
Smithienious <nhoang3b@yahoo.com.vn>
2020-04-10 12:50:43 +0700

    Added assignment 0 for PF

---
+++ CO1028 CC01/Assignment_0/arthur.cpp
@@ -0,0 +1,62 @@
+/*****Version 1.1***********/
+#include <stdio.h>
+#include <iostream.h>
+#include <fstream.h>
+#include <math.h>
+
+//read data from input file to corresponding variables
+//return 1 if successfully done, otherwise return 0
+int readFile(int& baseHP1,int& baseHP2,int& wp1,int& wp2,int& ground)
+{
+	char* file_name = "input.txt";
+	ifstream in;
+	in.open(file_name);
+	in >> baseHP1;
+	in >> wp1;
+	in >> baseHP2;
+	in >> wp2;
+	in >> ground;
+	in.close();
+	if (baseHP1 < 99 || baseHP1 > 999)
+		return 0;
+	if (wp1 < 0 || wp1 > 3)
+		return 0;
+	if (baseHP2 < 1 || baseHP2 > 888)
+		return 0;
+	if (wp2 < 0 || wp2 > 3)
+		return 0;
+	if (ground < 1 || ground > 999)
+		return 0;
+	in.close();
+	return 1;
+
+}
+
+void display(float fOut)
+//display value of fOut in format of 0.XX
+// no exception handled
+{
+	if (fOut == 1){
+		cout << fOut;
+	}
+	else{
+		char s[10];
+		sprintf(s,"%.2f",fOut);
+		cout << s;
+	}
+}
+
+
+int main(void)
+{
+	int baseHP1,baseHP2,wp1,wp2,ground;
+	float fOut = 0.0;
+	readFile(baseHP1,baseHP2,wp1,wp2,ground);
+	
+	//write your code here
+
+
+	display(fOut);
+
+	return 0;
+}
