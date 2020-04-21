e70e80c5402c1600279593b3bb5325439ae3c820
Smithienious <nhoang3b@yahoo.com.vn>
2020-04-21 21:47:59 +0700

    Completed project 1

--- MT1005/Video/Project_1/Project_1a.m
+++ MT1005/Video/Project_1/Project_1a.m
@@ -1,13 +1,13 @@
 hold on;
 grid on;
 rotate3d on;
-xlim([0 1.5]);
-ylim([0 1.5]);
-zlim([0 1.5]);
-
 xlabel('x');
 ylabel('y');
 zlabel('z');
+
+xlim([0 1.5]);
+ylim([0 1.5]);
+zlim([0 1.5]);
 
 t = 0:0.01:1;
 
@@ -30,17 +30,19 @@ y3 = [0 1 0];
 z3 = [0 0 1];
 
 x4 = t;
-y4 = sqrt(t);
-z4 = zeros(1, 101);
-x5 = t;
-y5 = sqrt(t);
-z5 = 1 - sqrt(t);
-x4 = [x4 x5];
-y4 = [y4 y5];
-z4 = [z4 z5];
+y4 = sqrt(x4);
+c1 = zeros(1, 101);
+c2 = 1 - sqrt(x4);
+
+curve1 = [x4; y4; c1];
+curve2 = [x4; y4; c2];
+X=[curve1(1, :), fliplr(curve2(1, :))];
+Y=[curve1(2, :), fliplr(curve2(2, :))];
+Z=[curve1(3, :), fliplr(curve2(3, :))];
 
 fill3(x1, y1, z1, 'c');
 fill3(x2, y2, z2, 'c');
 fill3(x3, y3, z3, 'c');
-mesh(x4, y4, z4, 'c');
+fill3(X, Y, Z, 'c');
+
 hold off;
