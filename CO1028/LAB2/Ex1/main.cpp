b87619cae10db9470324662329ea56469b0e58f8
Smithienious <nhoang3b@yahoo.com.vn>
2020-05-02 12:45:33 +0700

    Completed exercise 1

--- CO1028/LAB2/Ex1/main.cpp
+++ CO1028/LAB2/Ex1/main.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
-// matrix is two dimensional array. You should transposition matrix
-void transposition(int rows, int cols, int **matrix) {
-	#TODO
+void Fibonacci(int n) {
+    int arr[n + 1];
+    arr[0] = 0; arr[1] = 1; arr[2] = 1;
+
+    for (int i = 2; i <= n; i += 1)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    cout << arr[n] << endl;
 }
-int main(int narg, char** argv)
+
+int main(int argc, char** argv)
 {
 	ifstream ifs;
 	ifs.open(argv[1]);
-	
-	int rows;
-	int cols;
-	ifs >> rows;
-	ifs >> cols;
-	int** matrix = new int*[rows];
+	int a;
 	try
 	{
-		int i = 0;
-		int j = 0;
-		for (int i = 0; i < rows; i++) {
-			matrix[i] = new int[cols];
-			for (int j = 0; j < cols; j++) {
-				ifs >> matrix[i][j];
-			}
+		while (ifs >> a)
+		{
+			Fibonacci(a);
 		}
-		transposition(rows, cols, matrix);
 	}
 	catch (char const* s)
 	{
