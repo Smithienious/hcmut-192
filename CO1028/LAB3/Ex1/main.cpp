216d057a5f07c6b7088fe636b81a9347b888221c
Smithienious <nhoang3b@yahoo.com.vn>
2020-05-08 21:36:09 +0700

    Completed exercise 1

--- CO1028/LAB3/Ex1/main.cpp
+++ CO1028/LAB3/Ex1/main.cpp
@@ -5,7 +5,52 @@ using namespace std;
 
 void printOrder(char *str1, char *str2, char *str3)
 {
-	// TODO
+	if (strcmp(str1, str2) < 0)
+	{
+		if (strcmp(str1, str3) < 0)
+		{
+			cout << str1 << endl;
+			if (strcmp(str2, str3) < 0)
+			{
+				cout << str2 << endl;
+				cout << str3 << endl;
+			}
+			else
+			{
+				cout << str3 << endl;
+				cout << str2 << endl;
+			}
+		}
+		else
+		{
+			cout << str3 << endl;
+			cout << str1 << endl;
+			cout << str2 << endl;
+		}
+	}
+	else
+	{
+		if (strcmp(str2, str3) < 0)
+		{
+			cout << str2 << endl;
+			if (strcmp(str1, str3) < 0)
+			{
+				cout << str1 << endl;
+				cout << str3 << endl;
+			}
+			else
+			{
+				cout << str3 << endl;
+				cout << str1 << endl;
+			}
+		}
+		else
+		{
+			cout << str3 << endl;
+			cout << str2 << endl;
+			cout << str1 << endl;
+		}
+	}
 }
 
 int main(int argc, char **argv)
